b17c21051e2e207a0e0a8e3300a74cce7cfc778c
dongtuo9426 <dongtuo9426@163.com>
2022-09-25 22:41:27 +0800

    登录页  粒子效果  echarts图表

--- src/main.js
+++ src/main.js
@@ -7,6 +7,11 @@ import 'element-ui/lib/theme-chalk/index.css';
 import axios from "axios";
 Vue.use(ElementUI);
 Vue.prototype.$axios = axios
+import VueParticles from 'vue-particles'
+Vue.use(VueParticles)
+import * as echarts from 'echarts'
+Vue.prototype.$echarts = echarts
+
 
 Vue.config.productionTip = false
 
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="home">
-我是首页
+    <div id="id" style="width: 600px; height: 400px"></div>
   </div>
 </template>
 
@@ -9,6 +9,30 @@ export default {
   name: 'HomeView',
   components: {
 
-  }
+  },
+  mounted() {
+    this.init()
+  },
+  methods:{
+    init(){
+      var myChart = this.$echarts.init(document.getElementById('id'))
+      var option = {
+        xAxis: {
+          type: 'category',
+          data: ['1', '2', '3', '4', '5', '6', '7']
+        },
+        yAxis: {
+          type: 'value'
+        },
+        series: [
+          {
+            data: [15, 23, 24, 28, 35, 47, 60],
+            type: 'line'
+          }
+        ]
+      };
+      option && myChart.setOption(option);
+    }
+  },
 }
 </script>
