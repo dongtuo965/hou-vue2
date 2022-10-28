f769f1d3ad38799d474294f6261051609d924a8a
dongtuo9426 <dongtuo9426@163.com>
2022-10-28 22:23:25 +0800

    图表调整

--- src/views/WhiteName.vue
+++ src/views/WhiteName.vue
@@ -1,51 +1,5 @@
 <template>
-<<<<<<< HEAD
-  <div>
-    <div id="main" style="width: 600px;height:400px;"></div>
-  </div>
-</template>
-
-<script>
-  export default {
-    name: 'WhiteName',
-    data() {
-      return {}
-    },
-    mounted() {
-      this.init()
-    },
-    methods: {
-      init() {
-        // 基于准备好的dom，初始化echarts实例
-        var myChart = this.$echarts.init(
-          document.getElementById("main")
-        );
-        // 配置option选项
-        var option = {
-          xAxis: {
-            type: 'category',
-            data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-          },
-          yAxis: {
-            type: 'value'
-          },
-          series: [
-            {
-              data: [150, 230, 224, 218, 135, 147, 260],
-              type: 'line'
-            }
-          ]
-        };
-        // 把配置option选项用js放进dom节点
-        myChart.setOption(option);
-      }
-=======
-
-  <el-row>
-  <el-col :span="12"><div class="grid-content bg-purple" id="main" style="width: 600px;height:400px;"></div></el-col>
-  <el-col :span="12"><div class="grid-content bg-purple-light" id="tubiao" style="width: 600px;height:400px;"></div></el-col>
-</el-row>
-
+  <div id="main" style="width: 300px;height: 300px"></div>
 </template>
 
 <script>
@@ -86,92 +40,88 @@ export default {
       // 把配置option选项用js放进dom节点
       myChart.setOption(option);
     },
-    tubiao(){
-      var chartDom = document.getElementById('tubiao');
-var myChart = echarts.init(chartDom);
-var option;
-
-option = {
-  title: {
-    text: 'Temperature Change in the Coming Week'
-  },
-  tooltip: {
-    trigger: 'axis'
-  },
-  legend: {},
-  toolbox: {
-    show: true,
-    feature: {
-      dataZoom: {
-        yAxisIndex: 'none'
-      },
-      dataView: { readOnly: false },
-      magicType: { type: ['line', 'bar'] },
-      restore: {},
-      saveAsImage: {}
-    }
-  },
-  xAxis: {
-    type: 'category',
-    boundaryGap: false,
-    data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
-  },
-  yAxis: {
-    type: 'value',
-    axisLabel: {
-      formatter: '{value} °C'
-    }
-  },
-  series: [
-    {
-      name: 'Highest',
-      type: 'line',
-      data: [10, 11, 13, 11, 12, 12, 9],
-      markPoint: {
-        data: [
-          { type: 'max', name: 'Max' },
-          { type: 'min', name: 'Min' }
-        ]
-      },
-      markLine: {
-        data: [{ type: 'average', name: 'Avg' }]
-      }
-    },
-    {
-      name: 'Lowest',
-      type: 'line',
-      data: [1, -2, 2, 5, 3, 2, 0],
-      markPoint: {
-        data: [{ name: '周最低', value: -2, xAxis: 1, yAxis: -1.5 }]
-      },
-      markLine: {
-        data: [
-          { type: 'average', name: 'Avg' },
-          [
-            {
-              symbol: 'none',
-              x: '90%',
-              yAxis: 'max'
+    tubiao() {
+      var chartDom = document.getElementById('main');
+      var myChart = this.$echarts.init(chartDom);
+      var option = {
+        title: {
+          text: 'Temperature Change in the Coming Week'
+        },
+        tooltip: {
+          trigger: 'axis'
+        },
+        legend: {},
+        toolbox: {
+          show: true,
+          feature: {
+            dataZoom: {
+              yAxisIndex: 'none'
             },
-            {
-              symbol: 'circle',
-              label: {
-                position: 'start',
-                formatter: 'Max'
-              },
-              type: 'max',
-              name: '最高点'
+            dataView: {readOnly: false},
+            magicType: {type: ['line', 'bar']},
+            restore: {},
+            saveAsImage: {}
+          }
+        },
+        xAxis: {
+          type: 'category',
+          boundaryGap: false,
+          data: ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+        },
+        yAxis: {
+          type: 'value',
+          axisLabel: {
+            formatter: '{value} °C'
+          }
+        },
+        series: [
+          {
+            name: 'Highest',
+            type: 'line',
+            data: [10, 11, 13, 11, 12, 12, 9],
+            markPoint: {
+              data: [
+                {type: 'max', name: 'Max'},
+                {type: 'min', name: 'Min'}
+              ]
+            },
+            markLine: {
+              data: [{type: 'average', name: 'Avg'}]
             }
-          ]
+          },
+          {
+            name: 'Lowest',
+            type: 'line',
+            data: [1, -2, 2, 5, 3, 2, 0],
+            markPoint: {
+              data: [{name: '周最低', value: -2, xAxis: 1, yAxis: -1.5}]
+            },
+            markLine: {
+              data: [
+                {type: 'average', name: 'Avg'},
+                [
+                  {
+                    symbol: 'none',
+                    x: '90%',
+                    yAxis: 'max'
+                  },
+                  {
+                    symbol: 'circle',
+                    label: {
+                      position: 'start',
+                      formatter: 'Max'
+                    },
+                    type: 'max',
+                    name: '最高点'
+                  }
+                ]
+              ]
+            }
+          }
         ]
-      }
+      };
+      option && myChart.setOption(option)
     }
-  ]
-};
-
-option && myChart.setOption(option);
-
->>>>>>> 1f01a964b14ecbfaaec631a624fce467656c3560
     }
   }
 </script>
