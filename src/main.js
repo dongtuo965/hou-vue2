cf602ae3b55efe24981f3fe230e0be336b35009f
dongtuo9426 <dongtuo9426@163.com>
2022-11-09 21:09:26 +0800

    操作

--- src/main.js
+++ src/main.js
@@ -2,18 +2,20 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
-import axios from "axios";
-Vue.use(ElementUI);
-Vue.prototype.$axios = axios
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css'
+import axios from 'axios'
+import Antd from 'ant-design-vue'
+import 'ant-design-vue/dist/antd.css'
 import VueParticles from 'vue-particles'
-Vue.use(VueParticles)
+
 import * as echarts from 'echarts'
+Vue.use(ElementUI)
+Vue.use(Antd)
+Vue.prototype.$axios = axios
+Vue.use(VueParticles)
 Vue.prototype.$echarts = echarts
-
 Vue.config.productionTip = false
-
 new Vue({
   router,
   store,
--- src/views/ImportantPeople.vue
+++ src/views/ImportantPeople.vue
@@ -1,13 +1,71 @@
 <template>
   <div>
-    重点人员管理
+    <a-tree
+      :tree-data="treeData"
+      checkable
+      :autoExpandParent="true"
+      :defaultExpandAll="true"
+      @expand="onExpand"
+      @select="onSelect"
+      @check="onCheck"
+    >
+<!--      通过switcherIcon修改只能修改所有父级的图标,子级不受影响-->
+<!--      <a-icon slot="switcherIcon" type="form" />-->
+<!--      此时子级定义一个插槽来修改子级图标-->
+<!--      <a-icon slot="child" type="wifi" />-->
+<!--      <a-icon slot="haha" type="solution" />-->
+                 <span slot="hc">
+                 <img src="@/assets/logo.png" style="width:24px;height:24px">
+                </span>
+      <span slot="fj">
+                   666
+                </span>
+    </a-tree>
+
   </div>
 </template>
 
 <script>
-    export default {
-        name: "ImportantPeople"
+export default {
+  name: 'ImportantPeople',
+  data () {
+    return {
+      treeData: []
     }
+  },
+  mounted () {
+    this.getData()
+  },
+  methods: {
+    getData () {
+      this.$axios.get('http://localhost:8889/treeData')
+        .then((response) => {
+          // 处理成功情况
+          this.treeData = response.data
+          console.log(response)
+        })
+        .catch((error) => {
+          // 处理错误情况
+          console.log(error)
+        })
+        .then(function () {
+          // 总是会执行
+        })
+    },
+    onExpand (checkedKeys, info) {
+      console.log('onCheck', checkedKeys, info)
+    },
+    onSelect (selectedKeys, info) {
+      console.log('selected', selectedKeys, info)
+    },
+    onCheck (checkedKeys, info) {
+      console.log('onCheck', checkedKeys, info)
+    },
+    onChange (e) {
+      console.log(`checked = ${e.target.checked}`)
+    }
+  }
+}
 </script>
 
 <style scoped>
